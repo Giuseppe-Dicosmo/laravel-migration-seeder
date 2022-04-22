4bb79f87cf3a137b45bf24f6f4deab831a6b8852
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-04-22 20:12:03 +0200

    migrations

--- laravel-migration-seeder/database/migrations/2022_04_22_131529_create_trains_table.php
+++ laravel-migration-seeder/database/migrations/2022_04_22_131529_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 30);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->timeTz('orario_di_partenza', 0);
+            $table->timeTz('orario_di_arrivo', 0);
+            $table->smallInteger('codice_treno')->unsigned();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(0);
+            $table->boolean('cancellato')->default(0);
             $table->timestamps();
         });
     }
